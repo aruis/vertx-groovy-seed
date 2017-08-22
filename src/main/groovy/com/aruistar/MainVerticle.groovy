132b227707d5f48dd09c11528cb9319c6bfd22e5
aruis <lovearuis@gmail.com>
2017-08-22 11:44:43 +0800

    加入 logback日志功能

--- src/main/groovy/com/aruistar/MainVerticle.groovy
+++ src/main/groovy/com/aruistar/MainVerticle.groovy
@@ -20,9 +20,13 @@ class MainVerticle extends AbstractVerticle {
     void start(Future<Void> startFuture) throws Exception {
 
 
+        def did
+
         vertx.deployVerticle(DatabaseVerticle.newInstance(), { dbHandler ->
             if (dbHandler.succeeded()) {
 
+                did = dbHandler.result()
+                println(did)
                 vertx.deployVerticle(HttpVerticle.newInstance(), new DeploymentOptions().setConfig(new JsonObject([port: 8080])), { httpHandler ->
                     if (httpHandler.succeeded()) {
                         startFuture.complete()
@@ -35,6 +39,15 @@ class MainVerticle extends AbstractVerticle {
 
         })
 
+        vertx.setTimer(10 * 1000, {
+            vertx.undeploy(did, {
+
+            })
+        })
+
+        vertx.setTimer(15 * 1000, {
+            vertx.deployVerticle(DatabaseVerticle.newInstance())
+        })
 
     }
 }
